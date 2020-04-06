0fd2aff7df13eba781f28f0d32da52ab40add3c1
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-04-06 14:36:46 +0200

    Adding component scoped styles

--- userCard.js
+++ userCard.js
@@ -1,3 +1,17 @@
+// creating a template for shadow DOM
+const template = document.createElement('template');
+
+template.innerHTML = `
+    <style>
+        h3 {
+            color: coral;
+        }
+    </style>
+
+    <h3></h3>
+`;
+
+
 // THE BASICS OF CREATING A CUSTOM ELEMENT
 // first thing in creating a component is to create a class that extends
 // creating a custom element
@@ -6,11 +20,26 @@ class UserCard extends HTMLElement {
         // calling the constructor of HTMLElement with Super()
         super();
 
+        // to encapsulate styles only to the custom element
+        // we use shadow DOM
+        this.attachShadow({ mode: 'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        this.shadowRoot.querySelector('h3').innerText = this.getAttribute('name');
+
         // to insert something in our custom element we use the this keyword
         // this.innerHTML = `John Doe`;
 
         // passing an attribute into a custom element
-        this.innerHTML = `${this.getAttribute('name')}`;
+        // this.innerHTML = `
+        //     <style>
+        //         h3 {
+        //             color: coral;
+        //         }
+        //     </style>
+
+        //     <h3>${this.getAttribute(name)}</h3>
+        // `;
     }
 }
 
