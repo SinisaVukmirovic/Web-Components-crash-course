c9919aec4c0fc480e78a8324c3041968257f60d2
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2020-04-06 14:08:27 +0200

    Basics of creating a custom element, component

---
+++ userCard.js
@@ -0,0 +1,18 @@
+// THE BASICS OF CREATING A CUSTOM ELEMENT
+// first thing in creating a component is to create a class that extends
+// creating a custom element
+class UserCard extends HTMLElement {
+    constructor() {
+        // calling the constructor of HTMLElement with Super()
+        super();
+
+        // to insert something in our custom element we use the this keyword
+        this.innerHTML = `John Doe`;
+    }
+}
+
+// defining the custom element
+                        // name of the tag, class we want to connect it to
+window.customElements.define('user-card', UserCard);
+// =================================================
+
